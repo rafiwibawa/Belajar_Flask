3bb24346725421713627eac199b99e0161e7ec5f
rafiwibawa <rafinew1997@gmail.com>
2020-03-08 15:26:13 +0700

    Menambahkan folder static dan file style.css

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    search = request.args.get('search')
+    return render_template('index.html',search=search)
 
 @app.route('/profile/<username>')
 def show_profile(username):
     return render_template('profile.html', username=username)
+
+@app.route('/login', methods=['GET','POST'])
+def show_login():
+    if request.method == 'POST':
+        return 'Email kamu Adalah '+ request.form['email']
+
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
